954dc9896288efc9d80e44f729603770869dfada
Łukasz Kużyński <lukasz.kuzynski@gmail.com>
2019-08-04 21:21:59 +0200

    Logger now supports PALLAD_LOGGER_FORMAT and NODE_ENV env variables

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import * as _format from './format';
-import {createLogger} from "winston";
 
 export const format = _format;
-export const logger = createLogger({
-    format: process.env.PALLAD_LOGGER_FORMAT === 'json' ? format.json : format.humanReadable
-});
-
 export * from './setupProcess';
 export * from './createLoggerForComponent';
+export * from './logger';
---
+++ src/logger.ts
@@ -0,0 +1,23 @@
+import {createLogger, log} from "winston";
+import {format} from "./index";
+
+function getFormat() {
+    const loggerFormat = (process.env.PALLAD_LOGGER_FORMAT || '').toLowerCase();
+    if (loggerFormat === 'json') {
+        return 'json';
+    }
+
+    if (loggerFormat === 'human' || loggerFormat === 'human-readable') {
+        return 'human';
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+        return 'json';
+    }
+
+    return 'human';
+}
+
+export const logger = createLogger({
+    format: getFormat() === 'json' ? format.json : format.humanReadable
+});
